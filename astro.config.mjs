864e3b34bdcd5ed852a7f1b0bbd4147b9ed9849e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-08-23 03:40:44 +0200

    Add Sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind(), sitemap()]
 });
