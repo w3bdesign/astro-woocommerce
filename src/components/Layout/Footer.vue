997a07499a9a0988d90d08900dc19cfe1ac16c72
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-09-22 04:59:37 +0200

    Convert Footer to SFC Vue 3 component

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -3,18 +3,15 @@
     <footer
       class="px-6 text-center bg-white border border-gray-300 rounded-lg shadow-lg"
     >
-      <div class="p-6">Copyright reserved &copy; {{ todayDate }} Daniel / w3bdesign</div>
+      <div class="p-6">
+        Copyright reserved &copy; {{ todayDate }} Daniel / w3bdesign
+      </div>
     </footer>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Footer',
-  computed: {
-    todayDate() {
-      return new Date().getFullYear()
-    },
-  },
-}
+<script setup>
+import { computed } from "vue";
+
+const todayDate = computed(() => new Date().getFullYear());
 </script>
