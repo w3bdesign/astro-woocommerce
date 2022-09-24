a69c02f47efa82437cdcfe8b8a7a66522ddf1724
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-09-24 03:32:07 +0200

    Add Astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config'
-import vue from '@astrojs/vue'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
-import vercel from '@astrojs/vercel/serverless'
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), sitemap()],
+  integrations: [vue(), tailwind(), sitemap(), image()],
   output: 'server',
   adapter: vercel()
-})
+});
