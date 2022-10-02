d2db7b3496a79ca0d95fedf3baf95b18d09e7850
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-02 02:36:25 +0200

    @headlessui/vue

--- src/components/Header/MobileMenu.vue
+++ src/components/Header/MobileMenu.vue
@@ -1,26 +1,12 @@
 <template>
   <div class="relative">
-
-
-
     <Menu as="div" class="relative inline-block text-left">
       <div>
         <MenuButton
-          class="burger burger-squeeze burger-lines inline-flex w-full justify-center rounded-md  px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          class="burger burger-squeeze burger-lines inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
         >
-         
           <div class="burger-lines"></div>
-         
         </MenuButton>
-
-
-
-
-
-
-
-
-
       </div>
 
       <transition
@@ -35,132 +21,41 @@
           class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
         >
           <div class="px-1 py-1">
-            <MenuItem v-slot="{ active }">
-              <a href="/" > Home </a>
-            </MenuItem>
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-               
-                Duplicate
-              </button>
-            </MenuItem>
-          </div>
-          <div class="px-1 py-1">
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-               
-                Archive
-              </button>
-            </MenuItem>
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-               
-                Move
-              </button>
-            </MenuItem>
-          </div>
+            <ul>
+              <MenuItem v-slot="{ active }">
+                <li class="text-xl linkStyle">
+                  <a href="/"> Home </a>
+                </li>
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+                <li class="text-xl linkStyle">
+                  <a href="/products"> Products </a>
+                </li>
+              </MenuItem>
 
-          <div class="px-1 py-1">
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-               
-                Delete
-              </button>
-            </MenuItem>
+              <MenuItem v-slot="{ active }">
+                <li class="text-xl linkStyle">
+                  <a href="/categories"> Categories </a>
+                </li>
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+                <li class="text-xl linkStyle">
+                  <a href="/search"> Search </a>
+                </li>
+              </MenuItem>
+
+              <MenuItem v-slot="{ active }">
+                <li class="flex justify-center mt-6 text-lg linkStyleCart">
+                  <a href="/cart">
+                    <CartIcon />
+                  </a>
+                </li>
+              </MenuItem>
+            </ul>
           </div>
         </MenuItems>
       </transition>
     </Menu>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div
-     
-    >
-      <ul>
-        <li class="text-xl linkStyle">
-          <a href="/" @click="displayMobileMenu"> Home </a>
-        </li>
-        <li class="text-xl linkStyle">
-          <a href="/products" @click="displayMobileMenu"> Products </a>
-        </li>
-        <li class="text-xl linkStyle">
-          <a href="/categories" @click="displayMobileMenu"> Categories </a>
-        </li>
-        <li class="text-xl linkStyle">
-          <a href="/search" @click="displayMobileMenu"> Search </a>
-        </li>
-        <li class="flex justify-center mt-6 text-lg linkStyleCart">
-          <a href="/cart" @click="displayMobileMenu">
-            <CartIcon />
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div class="w-5/12 lg:hidden"></div>
-    <div class="flex flex-row w-2/12 px-2 my-2 lg:hidden">
-      <div class="self-center block w-full mr-4">
-        <button
-          id="nav-toggle"
-          class="mt-6 burger burger-squeeze"
-          aria-haspopup="true"
-          :aria-expanded="expandedMenu"
-          aria-controls="menu"
-          aria-label="Navigation"
-          :class="{ open: expandedMenu }"
-          @click="displayMobileMenu"
-        >
-          <div class="burger-lines"></div>
-        </button>
-      </div>
-    </div>
   </div>
 </template>
 
