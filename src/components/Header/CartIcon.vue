e89794b388cad8694edbf2dc1e212e1c6faab5a4
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-02 02:59:45 +0200

    Fix all links

--- src/components/Header/CartIcon.vue
+++ src/components/Header/CartIcon.vue
@@ -1,8 +1,44 @@
 <template>
-    <div>
-     Cart icon comes here
+  <div>
+    <div v-if="remoteError">
+      <span class="text-xl text-red-500"
+        >Error fetching cart. Please refresh the page.</span
+      >
     </div>
-  </template>
-  
+    <div v-else>
+      <NuxtLink to="/cart">
+        <transition name="cart">
+          <span
+            v-if="cartLength"
+            class="text-xl text-white no-underline lg:text-black is-active"
+          >
+            <img
+              alt="Cart icon"
+              class="h-12 ml-4 lg:ml-2"
+              aria-label="Cart"
+              src="../../../public/svg/Cart.svg"
+          /></span>
+        </transition>
+        <transition name="cart">
+          <div v-if="cartLength">
+            <span
+              class="absolute w-6 h-6 pb-2 ml-16 -mt-12 text-center text-white bg-black rounded-full lg:ml-14"
+            >
+              {{ cartLength }}
+            </span>
+            <span>Total: {{ subTotal }}</span>
+          </div>
+        </transition>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
 
-  
+<script setup>
+  // Default values for testing
+const remoteError = false
+
+const cartLength = 1
+
+const subTotal = 199
+</script>
