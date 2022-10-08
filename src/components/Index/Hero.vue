f7dc2b865446e460dd412bae4ffa9fa1acdfbcc5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-08 05:40:31 +0200

    Update Hero.vue

--- src/components/Index/Hero.vue
+++ src/components/Index/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img class="mx-auto" alt="Image slider slide #1" :src={heroImage} />
+    <img class="mx-auto" alt="Image slider slide #1" :src="heroImage" />
     <div
       class="flex flex-col items-start justify-center w-full mx-auto tracking-wide lg:w-1/2"
     >
