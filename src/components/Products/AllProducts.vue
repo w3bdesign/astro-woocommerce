da91b94eafb0c05d5790c8addd6bd803d1ab07e7
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-11 04:16:51 +0200

    Fix products page

--- src/components/Products/AllProducts.vue
+++ src/components/Products/AllProducts.vue
@@ -1,9 +1,15 @@
 <template>
   <Suspense>
-    <ShowAllProducts />
+    <template #default>
+      <ShowAllProducts />
+    </template>
+    <template #fallback>
+      <LoadingSpinner />
+    </template>
   </Suspense>
 </template>
 
 <script setup>
 import ShowAllProducts from "./ShowAllProducts.vue"
+import LoadingSpinner from "@/components/Spinner/LoadingSpinner.vue"
 </script>
