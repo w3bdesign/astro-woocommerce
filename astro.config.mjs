5872061412e9194b1de95516ba7a8b0bc1c541b5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-13 23:27:59 +0200

    Add site in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
-import image from '@astrojs/image';
-
+import { defineConfig } from "astro/config"
+import vue from "@astrojs/vue"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import vercel from "@astrojs/vercel/serverless"
+import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind(), sitemap(), image()],
-  output: 'server',
-  adapter: vercel()
-});
+  output: "server",
+  adapter: vercel(),
+  site: "https://astro-woocommerce.vercel.app"
+})
