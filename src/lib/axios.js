8fc24c678f5b62da1cfc2fd08db7a01a9a023002
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-17 04:39:41 +0200

    Test session headers

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,8 +4,12 @@ export async function fetchAxios(query, { variables } = {}) {
   const { PUBLIC_GRAPHQL_URL } = import.meta.env
 
   const headers = {
-    "content-type": "application/json"
+    "content-type": "application/json",
+    "woocommerce-session":
+      "Session eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd29vY29tbWVyY2UuZGZ3ZWIubm8iLCJpYXQiOjE2NjU5NzQwNjksIm5iZiI6MTY2NTk3NDA2OSwiZXhwIjoxNjY2MTQ2ODY5LCJkYXRhIjp7ImN1c3RvbWVyX2lkIjoidF9iMTkzODcwMGY2MGE3YzE0YjVjNGNkYTgwYjZhYzgifX0.MBkm6s2lWtopoaE1rTvLyFDbtFFB56Q3cYVKe5hTNcg"
   }
+
+  axios.defaults.withCredentials = true
 
   return axios({
     url: PUBLIC_GRAPHQL_URL,
