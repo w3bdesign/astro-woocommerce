b2a203b8279e114c9247f39a149d6373e6d69908
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-18 00:29:01 +0200

    Show cart length and total

--- src/components/Header/CartIcon.vue
+++ src/components/Header/CartIcon.vue
@@ -20,12 +20,13 @@
           /></span>
         </a>
       </transition>
+
       <transition name="cart">
         <div v-if="cartLength">
           <span
             class="absolute w-6 h-6 pb-2 ml-16 -mt-12 text-center text-white bg-black rounded-full lg:ml-14"
           >
-            Cart length: {{ cartLength }}
+            {{ cartLength }}
           </span>
           <span>Total: {{ subTotal }}</span>
         </div>
@@ -35,28 +36,27 @@
 </template>
 
 <script setup>
+import { ref } from "vue"
+
 import { getCart } from "@/graphql/queries/getCart"
 
 import { filteredVariantPrice } from "@/utils/functions"
 
 let testCart
 
-let cartLength //= 1
+let subTotal = ref("kr 0")
+
+let cartLength = ref(0)
 
 setInterval(async () => {
   testCart = await getCart()
 
-  console.log("testCart.contents.nodes[0]", testCart.contents.nodes[0])
-
   if (testCart.contents.nodes[0]) {
-    console.log("Nodes!")
-    cartLength = testCart.contents.nodes[0].quantity
+    cartLength.value = testCart.contents.nodes[0].quantity
+    subTotal.value = testCart.contents.nodes[0].subTotal
   }
-  //console.log("testCart length is:", testCart.contents.nodes[0].quantity)
 }, 6000)
 
 // Default values for testing
 const remoteError = false
-
-const subTotal = 199
 </script>
