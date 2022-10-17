f7c2155c77277ccca553dcb06ccaa4b24aac2cf3
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-17 05:08:38 +0200

    Store woocommerce-session in localStorage

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,8 +3,21 @@ import axios from "axios"
 export async function fetchAxios(query, { variables } = {}) {
   const { PUBLIC_GRAPHQL_URL } = import.meta.env
 
+  let woocommerceSession
+
+  woocommerceSession = undefined
+
+  if (localStorage["woocommerce-session"]) {
+    console.log("Exists!")
+    console.log(localStorage["woocommerce-session"])
+    woocommerceSession = localStorage["woocommerce-session"]
+  }
+
   const headers = {
-    "content-type": "application/json"/*,
+    "content-type": "application/json",
+    "woocommerce-session": `Session ${woocommerceSession}`
+
+    /*,
     "woocommerce-session":
       "Session eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd29vY29tbWVyY2UuZGZ3ZWIubm8iLCJpYXQiOjE2NjU5NzQwNjksIm5iZiI6MTY2NTk3NDA2OSwiZXhwIjoxNjY2MTQ2ODY5LCJkYXRhIjp7ImN1c3RvbWVyX2lkIjoidF9iMTkzODcwMGY2MGE3YzE0YjVjNGNkYTgwYjZhYzgifX0.MBkm6s2lWtopoaE1rTvLyFDbtFFB56Q3cYVKe5hTNcg"*/
   }
@@ -23,6 +36,12 @@ export async function fetchAxios(query, { variables } = {}) {
   })
     .then(response => {
       console.log("Response headers: ", response.headers["woocommerce-session"])
+
+      if (!localStorage["woocommerce-session"]) {
+        localStorage["woocommerce-session"] =
+          response.headers["woocommerce-session"]
+      }
+
       return response.data.data
     })
     .catch(error => {
