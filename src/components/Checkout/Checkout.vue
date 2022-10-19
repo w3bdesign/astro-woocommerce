5d6d9fc27b1cb78f01d01e8bb17ff16c4b5d8d98
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-20 01:03:20 +0200

    Simple form test

--- src/components/Checkout/Checkout.vue
+++ src/components/Checkout/Checkout.vue
@@ -1,9 +1,28 @@
-<template>This is Formkit form:</template>
+<template>
+  This is a form:
+
+  <form @submit="handleSubmit()">
+    <label htmlFor="email">Email</label>
+    <input type="text" name="email" id="email" required />
+
+    <label htmlFor="name">Name</label>
+    <input type="text" name="name" id="name" />
+
+    <label htmlFor="isPromoAllowed">Include promotional emails</label>
+    <input type="checkbox" name="isPromoAllowed" id="isPromoAllowed" />
+
+    <button type="submit">Sign me up</button>
+  </form>
+</template>
 
 <script setup>
 // https://github.com/bholmesdev/astro-zod-form-demo/blob/main/src/components/Form.tsx
 
 import { reactive } from "vue"
+
+const handleSubmit = () => {
+  alert("Submit")
+}
 
 const formData = reactive({
   firstName: "",
---
+++ src/pages/api/checkout.js
@@ -0,0 +1,18 @@
+// https://github.com/bholmesdev/astro-zod-form-demo/blob/main/src/pages/api/newsletter.ts
+
+export const post = async ({ request }) => {
+  const result = await request.formData()
+
+  if (result) {
+    // do stuff
+    console.log(result)
+    return new Response(null, { status: 200 })
+  } else {
+    return new Response(
+      JSON.stringify({
+        message: "Data is mal-form-ed (hehe)"
+      }),
+      { status: 400 }
+    )
+  }
+}
