b2104d8a65e63481ea53aa57254124674bde6e39
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-20 02:01:43 +0200

    Work on billing form

--- src/components/Checkout/Checkout.vue
+++ src/components/Checkout/Checkout.vue
@@ -1,18 +1,40 @@
 <template>
   This is a form:
 
-  <form @submit="handleSubmit()">
-    <label htmlFor="email">Email</label>
-    <input type="text" name="email" id="email" required />
+  <section>
+    <div class="container p-4 mx-auto mt-2 flex-container">
+      <form @submit="handleSubmit()">
+        <div class="w-64 mx-auto lg:w-1/2">
+          <div class="flex flex-wrap mt-2">
+            <div class="p-2 lg:w-1/2">
+              <label htmlFor="firstName">First name</label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                label="First name"
+                v-model="formData.firstName"
+                placeholder="First name"
+                required
+              />
 
-    <label htmlFor="name">Name</label>
-    <input type="text" name="name" id="name" />
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
 
-    <label htmlFor="isPromoAllowed">Include promotional emails</label>
-    <input type="checkbox" name="isPromoAllowed" id="isPromoAllowed" />
+              <label htmlFor="isPromoAllowed">Include promotional emails</label>
+              <input
+                type="checkbox"
+                name="isPromoAllowed"
+                id="isPromoAllowed"
+              />
 
-    <button type="submit">Sign me up</button>
-  </form>
+              <button type="submit">Sign me up</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </section>
 </template>
 
 <script setup>
