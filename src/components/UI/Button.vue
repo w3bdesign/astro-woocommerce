4d48c8effc43cdfcec38e7c89b09b087ca6389f7
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-20 03:06:50 +0200

    Button component

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button
+    class="px-4 py-2 font-bold bg-white border border-gray-400 border-solid rounded hover:bg-gray-400"
+  >
+    {{ props.text }}
+  </button>
+</template>
+
+<script setup>
+const props = defineProps(["text"])
+</script>
