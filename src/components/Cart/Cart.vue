854741be948c0706ae571e17b9d5f25225b47806
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-25 00:49:09 +0200

    Code cleanup

--- src/components/Cart/Cart.vue
+++ src/components/Cart/Cart.vue
@@ -1,27 +1,35 @@
 <template>
-  cartContent from component:
-  <pre>{{ cartContent }}</pre>
-  Cart length: {{ cartLength }} - Cart total: {{ subTotal }}
+  <div v-if="cartContent">
+    <pre>{{ cartContent }} </pre>
+    <br />
+    Cart length: {{ cartLength }}
+    <br />
+    Cart total: {{ subTotal }}
+  </div>
+  <div v-else>
+    <LoadingSpinner />
+  </div>
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref, onBeforeMount } from "vue"
 
 import { getCart } from "@/graphql/queries/getCart"
+
+import LoadingSpinner from "@/components/Spinner/LoadingSpinner.vue"
 
 let cartContent = ref()
 let subTotal = ref()
 let cartLength = ref(0)
 
-setTimeout(async () => {
+onBeforeMount(async () => {
   const cart = await getCart()
 
   if (cart && cart.contents.nodes[0]) {
     cartContent.value = cart.contents.nodes
-    console.log(cart.contents.nodes[0].quantity)
+
     cartLength.value = cart.contents.nodes[0].quantity
     subTotal.value = cart.contents.nodes[0].total
-    console.log("Total:", cart.contents.nodes[0].total)
   }
-}, 5000)
+})
 </script>
