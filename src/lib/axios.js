f3d295e826704a3023440d93fe847a2999f97f27
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-24 05:03:13 +0200

    Cleanup

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -14,10 +14,6 @@ export async function fetchAxios(query, { variables } = {}) {
   const headers = {
     "content-type": "application/json",
     "woocommerce-session": `Session ${woocommerceSession}`
-
-    /*,
-    "woocommerce-session":
-      "Session eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd29vY29tbWVyY2UuZGZ3ZWIubm8iLCJpYXQiOjE2NjU5NzQwNjksIm5iZiI6MTY2NTk3NDA2OSwiZXhwIjoxNjY2MTQ2ODY5LCJkYXRhIjp7ImN1c3RvbWVyX2lkIjoidF9iMTkzODcwMGY2MGE3YzE0YjVjNGNkYTgwYjZhYzgifX0.MBkm6s2lWtopoaE1rTvLyFDbtFFB56Q3cYVKe5hTNcg"*/
   }
 
   axios.defaults.withCredentials = true
