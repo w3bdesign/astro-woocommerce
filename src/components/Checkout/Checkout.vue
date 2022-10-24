1c23d737e4d9af9494cd91e4c240d175b252cc99
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-24 03:55:41 +0200

    Show error message

--- src/components/Checkout/Checkout.vue
+++ src/components/Checkout/Checkout.vue
@@ -5,16 +5,6 @@
         <div class="w-64 mx-auto lg:w-1/2">
           <div class="flex flex-wrap mt-2">
             <div class="p-2 lg:w-1/2">
-              Error:
-
-              <div
-                class="input-errors"
-                v-for="error of validate.firstName.$errors"
-                :key="error.$uid"
-              >
-                <div class="error-msg">{{ error.$message }}</div>
-              </div>
-
               <div v-for="field in BILLING_FIELDS">
                 <BaseInputField
                   :inputId="field.inputId"
@@ -22,6 +12,10 @@
                   :required="field.required"
                   v-model="formData[field.inputId]"
                 />
+                <span class="text-2xl text-red-500 font-bold">{{
+                  validate[field.inputId].$errors[0] &&
+                  validate[field.inputId].$errors[0].$message
+                }}</span>
               </div>
               formData data:
               <pre>{{ formData && JSON.stringify(formData) }}</pre>
@@ -52,22 +46,26 @@ const state = reactive({
 })
 
 const rules = {
-  firstName: { required }
+  firstName: { required },
+  lastName: { required },
+  address1: { required },
+  postcode: { required },
+  city: { required },
+  email: { required },
+  phone: { required }
 }
 
 let formData = ref({})
 
-const validate = useVuelidate(rules, state)
+//const validate = useVuelidate(rules, state)
+
+const validate = useVuelidate(rules, formData)
 
 validate.value.$validate()
-
-
 
 const handleSubmit = async () => {
   //const validate = useVuelidate(rules, state, { $lazy: true })
 
   validate.value.$validate()
-
-  console.log(validate.value.firstName.$error)
 }
 </script>
