6bd0c6327c029445e614003ce7f5d7d844c2f73e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-24 22:41:29 +0200

    Show cart total and length

--- src/components/Cart/Cart.vue
+++ src/components/Cart/Cart.vue
@@ -1,28 +1,27 @@
 <template>
-  Cart from component:
-
-  {{ cartContent && JSON.stringify(cartContent.contents) }}
+  cartContent from component:
+  <pre>{{ cartContent }}</pre>
+  Cart length: {{ cartLength }} - Cart total: {{ subTotal }}
 </template>
 
 <script setup>
+import { ref } from "vue"
 
 import { getCart } from "@/graphql/queries/getCart"
 
-let cartContent
-
-
-
-// setInterval
+let cartContent = ref()
+let subTotal = ref()
+let cartLength = ref(0)
 
 setTimeout(async () => {
-  cartContent = await getCart()
+  const cart = await getCart()
 
- 
+  if (cart && cart.contents.nodes[0]) {
+    cartContent.value = cart.contents.nodes
+    console.log(cart.contents.nodes[0].quantity)
+    cartLength.value = cart.contents.nodes[0].quantity
+    subTotal.value = cart.contents.nodes[0].total
+    console.log("Total:", cart.contents.nodes[0].total)
+  }
 }, 5000)
-
-
-
-
-
-
 </script>
