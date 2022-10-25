4eca45573f5b6433e03521d8aa5646f897a76009
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-25 05:28:08 +0200

    Show cart content

--- src/components/Cart/Cart.vue
+++ src/components/Cart/Cart.vue
@@ -1,10 +1,32 @@
 <template>
   <div v-if="cartContent">
-    <pre>{{ cartContent }} </pre>
-    <br />
-    Cart length: {{ cartLength }}
-    <br />
-    Cart total: {{ subTotal }}
+    <div
+      v-for="products in cartContent"
+      :key="products.id"
+      class="container mx-auto mt-4 flex-container"
+    >
+      <div v-if="displayRemove" class="item">
+        <span class="block mt-2 font-extrabold">Remove: <br /></span>
+        <span class="item-content"> Remove </span>
+      </div>
+
+      <div class="item">
+        <span class="block mt-2 font-extrabold">Name: <br /></span>
+        <span class="item-content">{{ products.product.name }}</span>
+      </div>
+
+      <div class="item">
+        <span class="block mt-2 font-extrabold">Quantity: <br /> </span>
+        <span class="item-content">
+          {{ products.quantity }}
+        </span>
+      </div>
+
+      <div class="item">
+        <span class="block mt-2 font-extrabold">Subtotal: <br /></span>
+        <span class="item-content"> {{ products.total }} </span>
+      </div>
+    </div>
   </div>
   <div v-else>
     <LoadingSpinner />
@@ -33,3 +55,28 @@ onBeforeMount(async () => {
   }
 })
 </script>
+
+<style scoped>
+.flex-container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  align-content: center;
+  max-width: 1380px;
+  @apply border border-gray-300 rounded-lg shadow;
+}
+
+.item {
+  @apply lg:m-2 xl:m-4 xl:w-1/6 lg:w-1/6 sm:m-2 w-auto;
+}
+
+.item-content {
+  @apply inline-block mt-4 w-20 h-12 md:w-full lg:w-full xl:w-full;
+}
+
+.removing {
+  @apply animate-spin cursor-not-allowed;
+}
+</style>
