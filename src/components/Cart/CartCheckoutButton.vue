f670ea8812d1bfc7a9e1187ce6c64f486961b8fd
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-30 04:53:42 +0100

    Styling cart and checkout

--- src/components/Cart/CartCheckoutButton.vue
+++ src/components/Cart/CartCheckoutButton.vue
@@ -1,7 +1,7 @@
 <template>
   <a href="/checkout">
     <button
-      class="w-48 h-12 px-4 py-2 mt-8 font-bold text-white bg-blue-500 rounded hover:bg-blue-800 ease-in-out duration-300"
+      class="w-48 h-12 px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-800 ease-in-out duration-300"
       :type="props.type"
     >
       Checkout
