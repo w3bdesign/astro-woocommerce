9a581acf323919456f81a2947455e9df8c9c9042
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-01 00:23:11 +0100

    More work with categories

--- src/graphql/queries/getAllCategories.js
+++ src/graphql/queries/getAllCategories.js
@@ -1,7 +1,7 @@
-import { fetchAPI } from "../../lib/api"
+import { fetchAxios } from "../../lib/axios"
 
-export async function getAllProducts() {
-  const data = await fetchAPI(`
+export async function getAllCategories() {
+  const data = await fetchAxios(`
   {
     productCategories(first: 20) {
       nodes {
