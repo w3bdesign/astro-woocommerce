fc1a2e26faf2c2be7e9d41274c6232acedb65206
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-21 04:44:21 +0100

    Replace .then with await

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,8 @@ import axios from "axios"
 export async function fetchAxios(query, { variables } = {}) {
   const { PUBLIC_GRAPHQL_URL } = import.meta.env
 
-  let woocommerceSession = undefined
+  let woocommerceSession
+  let axiosData
 
   if (localStorage["woocommerce-session"]) {
     woocommerceSession = localStorage["woocommerce-session"]
@@ -16,26 +17,29 @@ export async function fetchAxios(query, { variables } = {}) {
 
   axios.defaults.withCredentials = true
 
-  return axios({
-    url: PUBLIC_GRAPHQL_URL,
-    method: "POST",
-    headers: headers,
-    withCredentials: true,
-    data: {
-      query: query,
-      variables: variables
-    }
-  })
-    .then(response => {
-      if (!localStorage["woocommerce-session"]) {
-        localStorage["woocommerce-session"] =
-          response.headers["woocommerce-session"]
+  try {
+    axiosData = await axios({
+      url: PUBLIC_GRAPHQL_URL,
+      method: "POST",
+      headers: headers,
+      withCredentials: true,
+      data: {
+        query: query,
+        variables: variables
       }
-      return response.data.data
     })
-    .catch(error => {
+
+    if (!localStorage["woocommerce-session"]) {
+      localStorage["woocommerce-session"] =
+        axiosData.headers["woocommerce-session"]
+    }
+  } catch {
+    error => {
       if (import.meta.env.DEV) {
         console.log("Error: ", error)
       }
-    })
+    }
+  }
+
+  return axiosData.data.data
 }
